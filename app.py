82cc8e670b7f52f21c49d4b67d28f0a56dd7f67d
Timothy pesi <48859349+timothypesi@users.noreply.github.com>
2023-03-02 21:52:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def load_data(nrows):
 # Create a text element and let the reader know the data is loading.
 data_load_state.text("Done! (using st.cache_data)")
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+data = load_data(10)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done
     
